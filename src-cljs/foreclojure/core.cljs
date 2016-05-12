f45a463a596d6b55bb494198151654e620b26cab
Matt Kocubinski <mkocubinski@gmail.com>
2016-05-12 11:21:02 -0400

    Beautiful, drawing trees.

--- src-cljs/foreclojure/core.cljs
+++ src-cljs/foreclojure/core.cljs
@@ -26,7 +26,7 @@
 
 (defn draw-node [two n x y]
   (let [y (* y row-height)]
-    (println "draw" n "at" x y)
+    ;;(println "draw" n "at" x y)
     (. two makeCircle x y 20)
     (. two makeText (str n) x y)))
 
@@ -53,19 +53,21 @@
 (defn draw-any [t]
   (let [two (two)
         w 70
-        ys (explore t)]
+        ys (explore t)
+        seen (atom (zipmap (keys ys) (repeat 0)))]
+    (println "explored" ys)
     ((fn step [n x y]
-       (if (coll? n)
-         (let [[p l r] n
-               dx (* (. two -width) (- (/ 1 (+ (ys y) 1)) (/ 1 (+ (ys y) 2))))
-               walk (fn [n nx ny]
-                      (draw-line two x (* y row-height) nx (* ny row-height))
-                      (step n nx ny))]
-           ;; find drawing general cases
-           (walk p x y)
-           (and l (walk l (- x dx) (inc y)))
-           (and r (walk r (+ x dx) (inc y))))
-         (draw-node two n x y)))
+       (doseq [node n]
+         (if-not (coll? node)
+           (draw-node two node x y)
+           (let [ny (inc y)
+                 lx (@seen ny)
+                 nx (* (. two -width) (/ (inc lx) (inc (ys ny))))]
+             (swap! seen update ny inc)
+             (draw-line two x (* y row-height) nx (* ny row-height))
+             (step node nx ny)))
+         )
+       )
      t (/ (. two -width) 2) 1)
     (. two update)))
 
@@ -76,10 +78,10 @@
        ['b]]
       [4
        ['c]
-       ['d]]]
+       ]]
    [5
     [6
-     ['e]
+
      ['f]]
     [7
      ['g]
